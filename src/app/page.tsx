0e67899a2d6a8cdbbcb9f66af8fad94a90731eff
khoalt <khoalt@runsystem.net>
2026-03-04 08:34:28 +0700

    feat: Force dynamic rendering for order and product APIs and the home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import prisma from '@/lib/prisma';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const [totalOrders, shippingOrders, totalProducts] = await Promise.all([
     prisma.order.count(),
